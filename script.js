e49b6d0b12206eff0af47f63a650677156764c49
SashAwl <aleks.shilova@gmail.com>
2022-06-29 15:52:54 +0300

    hw_5: changed script.js: used vue for code

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-];
-
 const BASE_URL = "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
 const URL_PRODUCTS = `${BASE_URL}/catalogData.json`
 
@@ -24,50 +17,6 @@ function service(url) {
 
 }
 
-class GoodsItem {
-    constructor({ product_name = "No name", price = "0" }) {
-        this.product_name = product_name;
-        this.price = price;
-    }
-
-    render() {
-        return `<div class="goods-item"><h3>${this.product_name}</h3><p>${this.price}</p></div>`;
-    }
-}
-
-class GoodsList {
-    list = [];
-    listFiltered = [];
-    fetchData() {
-        return service(URL_PRODUCTS).then((data) => {
-            this.list = data;
-            this.listFiltered = data;
-        });
-    }
-
-    filter(str) {
-        this.listFiltered = this.list.filter(({ product_name }) => {
-            return (new RegExp(str, "i")).test(product_name);
-        })
-    }
-
-    render() {
-        let goodsList = this.listFiltered.map((item) => {
-            const goodsItem = new GoodsItem(item);
-            return goodsItem.render();
-        })
-        document.querySelector('.goods-list').innerHTML = goodsList.join('');
-    }
-
-    getSum() {
-        const sumPrice = this.list.reduce((sum, item) => {
-            return sum + item.price;
-        }, 0);
-        return sumPrice;
-    }
-
-}
-
 URL_BASKETGOODS = `${BASE_URL}/getBasket.json`
 
 class BasketGoods {
@@ -79,17 +28,44 @@ class BasketGoods {
     }
 }
 
-const goodsList = new GoodsList();
-goodsList.fetchData().then(() => {
-    goodsList.render();
-});
+window.onload = () => {
+    const app = new Vue({
+        el: "#root",
+        data: {
+            list: [],
+            searchValue: '',
+            isVisibleCart: false,
+        },
 
-const basketGoods = new BasketGoods();
-basketGoods.fetchBasket();
+        mounted() {
+            service(URL_PRODUCTS).then((data) => {
+                this.list = data;
+            });
+        },
 
-document.getElementsByClassName("search-button")[0].addEventListener('click', () => {
-    const input = document.getElementsByClassName("search-value")[0];
+        methods: {
+            openCart() {
+                this.isVisibleCart = true;
+            },
 
-    goodsList.filter(input.value);
-    goodsList.render();
-})
+            closeCart() {
+                this.isVisibleCart = false;
+            }
+        },
+
+        computed: {
+            getSum() {
+                const sumPrice = this.list.reduce((sum, item) => {
+                    return sum + item.price;
+                }, 0);
+                return sumPrice;
+            },
+
+            filter() {
+                return this.list.filter(({ product_name }) => {
+                    return product_name.match(new RegExp(this.searchValue, "gui"));
+                })
+            }
+        }
+    });
+}
