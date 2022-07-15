4f8dae437f9339eb2b81997035414b2cde32b24b
SashAwl <aleks.shilova@gmail.com>
2022-07-15 10:05:58 +0300

    hw_8: created button 'addProduct'

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { writeFile, readFile } from 'fs/promises';
+import bodyParser from 'body-parser';
 
 const BASKET = './public/basket-goods.json';
 const GOODS = './public/goods.json';
@@ -21,9 +22,8 @@ const readGoods = () => readFile(GOODS, 'utf-8')
         return JSON.parse(stringFile)
     });
 
-app.get('/basket', (req, res) => {
-
-    Promise.all([readBasket(), readGoods()])
+const getReforBasket = () => {
+    return Promise.all([readBasket(), readGoods()])
         .then(([basketList, goodsList]) => {
             return basketList.map((basketItem) => {
                 const goodsItem = goodsList.find(({ id_product: _goodsId }) => {
@@ -35,9 +35,43 @@ app.get('/basket', (req, res) => {
                 }
             });
         })
-        .then((result) => {
-            res.send(JSON.stringify(result))
-        })
+    return result;
+}
+
+app.post('/goods', (res, req) => {
+    readBasket().then((basket) => {
+
+        const basketItem = basket.find(item => item.id_product === res.body.id_product)
+
+        if (!basketItem) {
+            basket.push({
+                id_product: res.body.id_product,
+                count: 1
+            })
+        } else {
+            basket = basket.map(basketItem => {
+                if (basketItem.id_product === res.body.id_product) {
+                    return {
+                        ...basketItem,
+                        count: basketItem.count + 1
+                    }
+                } else {
+                    return basketItem
+                }
+            })
+        }
+        writeFile(BASKET, JSON.stringify(basket)).then(() => {
+            getReforBasket()
+        }).then((result) => {
+            req.send(result)
+        });
+    });
+});
+
+app.get('/basket', (res, req) => {
+    getReforBasket().then((result) => {
+        req.send(JSON.stringify(result))
+    })
 });
 
 app.listen('8000', () => {
